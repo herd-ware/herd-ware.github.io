e3c77208ccc4100b416ae0cb754594b02ba05823
Mathieu Escouteloup <mathieu.escouteloup@gmail.com>
2023-02-25 15:06:02 +0100

    Adds generated site

---
+++ docs/index.search.js
@@ -0,0 +1,58 @@
+var relearn_search_index = [
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Contributors",
+    "uri": "/more/contrib/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Publications",
+    "uri": "/more/publi/index.html"
+  },
+  {
+    "content": "Table of contents  Table of contents Digital system design and tools Instruction Set Architecture Microarchitecture \u0026 processor design  Digital system design and tools [Chisel]: Chisel/FIRRTL Hardware Compiler Framework   Available link   Resume: Here is the website of the Chisel3 language. Differents informations about the language or the FIRRTL compiler can be found.   [Verilator]: Verilator   Available link   Resume:   Instruction Set Architecture [Patt2017]: Computer organization and design: the hardware/software interface , RISC-V Edition  Patterson, David A and Hennessy, John L 2017   Resume:   [Wate2019]: The RISC-V Instruction Set Manual , Volume I: Unprivileged ISA  Waterman, Andrew and Asanovic, Krste December 2019 Available link   Resume: This is the official RISC-V specification. It describes the different instruction set bases and the major extensions.   [Wate2021]: The RISC-V Instruction Set Manual , Volume II: Privileged Architecture  Waterman, Andrew and Asanovic, Krste December 2021 Available link   Resume: This is the official RISC-V specification. It describes the different instruction set extensions for privileges support.   Microarchitecture \u0026 processor design [Henn2011]: Computer architecture: a quantitative approach  Hennessy, John L and Patterson, David A 2011   Resume:   ",
+    "description": "",
+    "tags": null,
+    "title": "References",
+    "uri": "/refs/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Research topics",
+    "uri": "/more/research/index.html"
+  },
+  {
+    "content": " BHT: Branch History Table  Definition:  BTB: Branch Target Buffer  Definition:  EDA: Electronic Design Automation  Definition:  ISA: Instruction Set Architecture  Definition:  NLP: Next Line Predictor  Definition:  RAS: Return Address Stack  Definition:  See also:  RSB  RSB: Return Stack Buffer  Definition:  See also:  RAS  ",
+    "description": "",
+    "tags": null,
+    "title": "Glossary",
+    "uri": "/gls/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "HerdWare",
+    "uri": "/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
